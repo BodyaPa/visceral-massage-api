75c444d138468da3a1b42e845e5245d1c1c443cc
bohdanpa <bogdanpachenko@gmail.com>
2025-08-05 21:11:23 +0300

    The basis for building a project. Added: 1. Empty folder templates for flexible work with them. 2. The first migration for the test. 3. Starting the postgresqls “VMLDB” database.

---
+++ src/main/java/com/example/visceralmassageapi/VisceralMassageApiApplication.java
@@ -0,0 +1,18 @@
+package com.example.visceralmassageapi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VisceralMassageApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VisceralMassageApiApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "VML") String name) {
+        return String.format("Hello I'm %s!", name);
+    }
+}
---
+++ src/test/java/com/example/visceralmassageapi/VisceralMassageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visceralmassageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisceralMassageApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
